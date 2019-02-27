cc1840458b13de16952a9738f748a48ca6e2e027
SankalpSh <sankalpsh98@gmail.com>
2019-02-27 15:16:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const url = require('url');
 
 function boot() {
   win = new BrowserWindow()
-  win.loadURL(url.format({
-    pathname : 'index.html',
-    slashes : true
-  }))
+   win.loadURL('file://' + __dirname + '/index.html');
 }
 
 app.on('ready',boot);
